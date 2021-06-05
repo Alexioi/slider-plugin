79dbcdaf60ae597560a0de3b11a15fc0734ba757
Alexey Ivanov <alexioi1993@gmail.com>
2021-06-05 12:02:23 +0300

    написал стили для слайдера

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+import "./style.scss";
+
+import Model from "./MVP/model";
+import View from "./MVP/view";
+
+(function ($) {
+  jQuery.fn.test = function () {
+    this.append(
+      `
+      <div class='slider'>
+        <div class='slider__line'>
+          <span class='slider__dot'></span>
+        </div>
+        <div class='slider__line slider__line_align-right'>
+          <span class='slider__dot slider__dot_align-right'></span>
+        </div>  
+      </div>  
+      `
+    );
+  };
+})(jQuery);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "plugin.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
