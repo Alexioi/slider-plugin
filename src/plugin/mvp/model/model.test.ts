7ca23f04ee5f85a82be554340206af716269631b
Alexey Ivanov <alexioi1993@gmail.com>
2021-09-01 14:56:18 +0300

    настроил jest, написал первый тест

--- src/plugin/mvp/model/model.test.ts
+++ src/plugin/mvp/model/model.test.ts
@@ -1,5 +1,41 @@
-// import Model from './model';
+import Model from './model';
 
-// import { IOptions } from '../interfaces/interfaces';
+describe('Model testing', () => {
+  const model = new Model({
+    isRange: true,
+    isVertical: false,
+    hasTip: true,
+    hasScale: true,
+    step: 10,
+    min: 0,
+    max: 100,
+    from: 40,
+    to: 70,
+  });
 
-// describe('Model', () => {});
+  test('update valid options', () => {
+    model.updateOptions({
+      isRange: false,
+      isVertical: true,
+      hasTip: false,
+      hasScale: false,
+      step: 15,
+      min: -100,
+      max: 1000,
+      from: 400,
+      to: 700,
+    });
+    const options = model.getOptions();
+    expect(options).toEqual({
+      isRange: false,
+      isVertical: true,
+      hasTip: false,
+      hasScale: false,
+      step: 15,
+      min: -100,
+      max: 1000,
+      from: 400,
+      to: 700,
+    });
+  });
+});
