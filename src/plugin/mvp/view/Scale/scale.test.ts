ebde20cc573b6bcd2ea981a9f190e0a110804fe2
Ivanov Alexey <ivanovdev@disroot.org>
2022-07-09 17:52:25 +0300

    added test for scale

--- src/plugin/mvp/view/Scale/scale.test.ts
+++ src/plugin/mvp/view/Scale/scale.test.ts
@@ -13,18 +13,37 @@ const state = { ...sliderOptions.defaultConfig };
 const scale = new Scale(node, state, eventEmitter);
 scale.render();
 const scaleNode = node.querySelector('div');
-const markNodes = node.querySelectorAll('span');
+let markNodes = node.querySelectorAll('span');
 
-describe('Шкала и элементы шкалы', () => {
-  test('должены отображаться с нужными классами', () => {
+describe('Шкала', () => {
+  test('и элементы шкалы должны отображаться с нужными классами', () => {
     expect(scaleNode?.classList.contains('slider__scale')).toBeTruthy();
     markNodes.forEach((markNode) => {
       expect(markNode.classList.contains('slider__mark')).toBeTruthy();
     });
   });
-  console.log(node?.innerHTML);
 
-  test('должены удаляться', () => {
+  test('должна содержать верное количество элементов при разных значениях длины ', () => {
+    const testValues = (length: number, markValues: string[]): void => {
+      Object.defineProperty(HTMLElement.prototype, 'offsetWidth', {
+        configurable: true,
+        value: length,
+      });
+      scale.render();
+      markNodes = node.querySelectorAll('span');
+
+      markNodes.forEach((markNode, key) => {
+        expect(markNode.innerText).toEqual(markValues[key]);
+      });
+    };
+
+    testValues(2000, ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']);
+    testValues(700, ['0', '20', '40', '60', '80', '100']);
+    testValues(400, ['0', '33', '66', '100']);
+    testValues(100, ['0', '100']);
+  });
+
+  test('должна удаляться', () => {
     scale.destroy();
     const emptyDivNode = node.querySelector('div');
     const emptySpanNode = node.querySelector('div');
