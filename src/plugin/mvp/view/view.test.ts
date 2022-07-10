a49f8ff52af1308ca0a0d1b47c424adc098c6d67
Ivanov Alexey <ivanovdev@disroot.org>
2022-07-10 11:27:05 +0300

    remove coments in view tests

--- src/plugin/mvp/view/view.test.ts
+++ src/plugin/mvp/view/view.test.ts
@@ -7,11 +7,9 @@ import sliderOptions from '../../app/sliderOptions';
 import EventEmitter from '../../EventEmitter/EventEmitter';
 import Tip from './Tip/Tip';
 import Runner from './Runner/Runner';
-import Range from './Range/Range';
 import Scale from './Scale/Scale';
 
 jest.mock('../../EventEmitter/EventEmitter');
-jest.mock('./Range/Range.ts');
 jest.mock('./Runner/Runner.ts');
 jest.mock('./Scale/Scale.ts');
 jest.mock('./Tip/Tip.ts');
@@ -60,40 +58,4 @@ describe('Вид', () => {
     // @ts-ignore
     expect(Tip.mock.instances[0].destroy.mock.calls.length).toEqual(1);
   });
-  //   test('должен иметь верное смещение', () => {
-  //     expect(runnerNode?.style.left).toEqual('40%');
-  //   });
-  //   test('не должен содержать класса targeted, если выбран другой ползунок', () => {
-  //     expect(runnerNode?.classList.contains('slider__runner_targeted')).not.toBeTruthy();
-  //   });
-  //   test('должен содержать класса targeted, если выбран этот ползунок', () => {
-  //     target.value = 'from';
-  //     runner.render();
-  //     expect(runnerNode?.classList.contains('slider__runner_targeted')).toBeTruthy();
-  //   });
-  //   test('должен менять ориентацию', () => {
-  //     state.isVertical = true;
-  //     runner.render();
-  //     expect(runnerNode?.style.top).toEqual('40%');
-  //   });
-  //   test('должен не отправлять событие в event emitter, если не было перемещение курсора', () => {
-  //     runnerNode?.dispatchEvent(eventPointerDown);
-  //     document.dispatchEvent(eventPointerUp);
-  //     // @ts-ignore
-  //     const mockEventEmitterInstance = EventEmitter.mock.instances[0];
-  //     const mockEmit = mockEventEmitterInstance.emit;
-  //     expect(mockEmit.mock.calls[0]).toBeUndefined();
-  //   });
-  //   test('должен вызывать событие в event emitter, если было перемещение курсора и менять таргет', () => {
-  //     target.value = 'to';
-  //     runnerNode?.dispatchEvent(eventPointerDown);
-  //     document.dispatchEvent(eventPointerMove);
-  //     // @ts-ignore
-  //     const mockEventEmitterInstance = EventEmitter.mock.instances[0];
-  //     const mockEmit = mockEventEmitterInstance.emit;
-  //     expect(mockEmit.mock.calls[0]).toEqual([
-  //       'ChangedRunnerPosition',
-  //       { position: { x: NaN, y: NaN }, type: 'from' },
-  //     ]);
-  //   });
 });
