5f409c615d55d7b061ea651238dfffaed5e730f9
Ivanov Alexey <ivanovdev@disroot.org>
2022-07-10 18:41:55 +0300

    added tests for event emitter

---
+++ src/plugin/EventEmitter/eventEmitter.test.ts
@@ -0,0 +1,34 @@
+import EventEmitter from './EventEmitter';
+
+const eventEmitter = new EventEmitter();
+
+const makeCounter = () => {
+  let i = 0;
+  return (print?: true) => {
+    if (print) {
+      return i;
+    }
+    i += 1;
+    return i;
+  };
+};
+
+const counter = makeCounter();
+
+describe('Оповещатель', () => {
+  test('должен вызывать подписанную функцию', () => {
+    // @ts-ignore
+    eventEmitter.subscribe('testEvent', counter);
+    // @ts-ignore
+    eventEmitter.emit({ eventName: 'testEvent' });
+    expect(counter(true)).toEqual(1);
+  });
+
+  test('должен не вызывать функцию после отписки', () => {
+    // @ts-ignore
+    eventEmitter.unsubscribe('testEvent', counter);
+    // @ts-ignore
+    eventEmitter.emit({ eventName: 'testEvent' });
+    expect(counter(true)).toEqual(1);
+  });
+});
