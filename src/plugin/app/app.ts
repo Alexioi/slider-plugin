717cd66ad35cd88d2d9813aea6f3a2a6b6fc5d1f
Ivanov Alexey <ivanovdev@disroot.org>
2022-08-06 10:53:19 +0300

    refactoring class App

--- src/plugin/app/app.ts
+++ src/plugin/app/app.ts
@@ -6,27 +6,18 @@ import sliderOptions from './sliderOptions';
 import { IConfig, ICallbacks, IOptions } from '../types/types';
 
 class App {
-  private callbacks: ICallbacks;
+  private callbacks!: ICallbacks;
 
-  private model: Model;
+  private model!: Model;
 
-  private view: View;
+  private view!: View;
 
-  private presenter: Presenter;
+  private presenter!: Presenter;
 
-  private eventEmitter: EventEmitter;
+  private eventEmitter!: EventEmitter;
 
   constructor(node: HTMLElement, config?: IConfig) {
-    this.callbacks = { ...sliderOptions.callbacks };
-
-    this.eventEmitter = new EventEmitter();
-    const options: IOptions = JSON.parse(JSON.stringify({ ...sliderOptions.defaultConfig }));
-    this.model = new Model(options, this.eventEmitter);
-    this.view = new View(node, options, this.eventEmitter);
-    this.presenter = new Presenter(this.view, this.model, this.eventEmitter);
-    this.attachEventEmitters();
-
-    this.update(config);
+    this.init(node, config);
   }
 
   public update(config?: IConfig): void {
@@ -43,6 +34,19 @@ class App {
 
   public getOptions(): IOptions {
     return this.presenter.getOptions();
+  }
+
+  private init(node: HTMLElement, config?: IConfig): void {
+    this.callbacks = { ...sliderOptions.callbacks };
+    const options: IOptions = JSON.parse(JSON.stringify({ ...sliderOptions.defaultConfig }));
+
+    this.eventEmitter = new EventEmitter();
+    this.model = new Model(options, this.eventEmitter);
+    this.view = new View(node, options, this.eventEmitter);
+    this.presenter = new Presenter(this.view, this.model, this.eventEmitter);
+    this.attachEventEmitters();
+
+    this.update(config);
   }
 
   private attachEventEmitters(): void {
