539e8601550b92270a74125213c0cc6ad571beba
Ivanov Alexey <ivanovdev@disroot.org>
2023-08-03 13:51:19 +0300

    add test for tip

--- src/plugin/App/mvp/View/subViews/Tip/tip.test.ts
+++ src/plugin/App/mvp/View/subViews/Tip/tip.test.ts
@@ -106,4 +106,71 @@ describe('Tip', () => {
     expect(tipBoth.classList.contains(cssSelectors.hiddenTip)).toBeFalsy();
     expect(tipTo.classList.contains(cssSelectors.hiddenTip)).toBeTruthy();
   });
+
+  it('should handle pointerdown runner', () => {
+    tip.render({ hasTip: true, isRange: true });
+
+    const tipLine = document.querySelector(`.${cssSelectors.tipLine}`);
+    const tipNodeList = tipLine?.querySelectorAll(`.${cssSelectors.tip}`);
+
+    if (typeof tipNodeList === 'undefined') {
+      fail('tipNodeList equal undefined');
+    }
+
+    const [tipFrom, tipBoth, tipTo] = tipNodeList;
+
+    if (!(tipFrom instanceof HTMLDivElement)) {
+      fail('tipFrom is not HTMLDivElement');
+    }
+    if (!(tipBoth instanceof HTMLDivElement)) {
+      fail('tipBoth is not HTMLDivElement');
+    }
+    if (!(tipTo instanceof HTMLDivElement)) {
+      fail('tipTo is not HTMLDivElement');
+    }
+
+    const pointerDownEvent = new MouseEvent('pointerdown', {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    });
+
+    const pointerMoveEvent = new MouseEvent('pointermove', {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+      clientX: 100,
+      clientY: 200,
+    });
+    const pointerUpEvent = new MouseEvent('pointerup', {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    });
+
+    tip.subscribe('ChangeRunnerPosition', ({ type }) => {
+      if (type === 'from') {
+        expect(type).toEqual('from');
+        return;
+      }
+
+      expect(type).toEqual('to');
+    });
+
+    tip.subscribe('ChangeNearRunnerPosition', () => {
+      expect(true).toBeTruthy();
+    });
+
+    tipFrom.dispatchEvent(pointerDownEvent);
+    tipFrom.dispatchEvent(pointerMoveEvent);
+    tipFrom.dispatchEvent(pointerUpEvent);
+
+    tipBoth.dispatchEvent(pointerDownEvent);
+    tipBoth.dispatchEvent(pointerMoveEvent);
+    tipBoth.dispatchEvent(pointerUpEvent);
+
+    tipTo.dispatchEvent(pointerDownEvent);
+    tipTo.dispatchEvent(pointerMoveEvent);
+    tipTo.dispatchEvent(pointerUpEvent);
+  });
 });
